d0e8aa835f48f457e7d2f0cbdbf9b9cd12aafefa
Kolin63 <theverylastgoodname@gmail.com>
2026-05-08 11:01:59 -0400

    Refactor code
    Now, there is only an index.html and a main.js because JavaScript is
    silly and it is hard to include code from other files

---
+++ main.js
@@ -0,0 +1,54 @@
+// overload console.log so we can print in python server
+const oldLog = console.log;
+console.log = function(...args) {
+    // send log data to python
+    fetch('/log', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(args)
+    });
+    // but still send it to the console log
+    oldLog.apply(console, args);
+};
+
+class MenuScene extends Phaser.Scene {
+    constructor() {
+        super("MenuScene");
+    }
+    create() {
+        this.cameras.main.setBackgroundColor("#0000FF");
+        this.add.text(400, 120, "CELL GAME", {
+            fontSize: "80px",
+            color: "#FFFF00"
+        }).setOrigin(0.5);
+        this.createButton(400, 280, "Start Game", () => {
+            console.log("The game has been started");
+        });
+        this.createButton(400, 360, "Load the Game", () => {
+            console.log("This game has been saved");
+        });
+    }
+
+    createButton(x, y, text, callback) {
+        const btn = this.add.text(x, y, text, {
+            fontSize: "40px",
+            backgroundColor: "#000",
+            color: "#FFFF00",
+            padding: { x: 20, y: 10 }
+        }).setOrigin(0.5);
+        btn.setInteractive({ useHandCursor: true });
+        btn.on('pointerdown', callback);
+        btn.on('pointerover', () => btn.setStyle({ backgroundColor: "#333" }));
+        btn.on('pointerout', () => btn.setStyle({ backgroundColor: "#000" }));
+
+        return btn;
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [MenuScene]
+};
+new Phaser.Game(config);
